4bcb9020b557e8f31e0e6c46050c49e2e584bbe8
Jason Christian <70122753+jasonchriscodes@users.noreply.github.com>
2023-01-23 14:42:13 +1300

    Change pbe to freightdesk mail

--- lib/mail.py
+++ lib/mail.py
@@ -6,11 +6,15 @@ class Mailing:
 
     def __init__(self,type,useremail,password):
         if(type=="gmail"):
-            self.s = smtplib.SMTP("smtp.gmail.com",465)
+            print("before")
+            self.s = smtplib.SMTP("mail.freightdesk.co.nz",465)
+            print("test: self.s"+self.s)
             self.s.starttls()
             self.s.login(useremail,password)
+            print("test: self.s login:"+self.s.login(useremail,password))
             self.msg = MIMEMultipart('alternative')
             self.msg['From'] = useremail
+            print("Your message has been sent.")
 
     def close(self):
         self.s.quit()
@@ -25,6 +29,7 @@ class Mailing:
         part = MIMEText(text, type)
         self.msg.attach(part)
         if(self.s.sendmail(self.msg['From'], [you,carbCopy], self.msg.as_string())):
+            print("Your message has been sent.")
             return 1
         else:
             return 0
@@ -34,7 +39,9 @@ class Mailing:
 
     def sendHTMLMail(self,you,carbCopy,text): 
         if(self.sendMail('html',you,carbCopy,text)):
+            print("Your message has been sent.")
             return 1
         else:
             return 0
+    print("mailing")
 
--- trymail.py
+++ trymail.py
@@ -1,7 +1,8 @@
 from lib.mail import Mailing
 
-m = Mailing("gmail","bpe.automation@gmail.com","wElcome@12345")
+m = Mailing("gmail","info@freightdesk.co.nz","Kecapasin23!")
 m.setSubject("Processing POs")
 html = "<html><head></head><body>Hi Srinivas!<br/><br/>We are starting email processing now, please keep things <b>ready</b>.<br/><br/>Thank You,<br/>Regards,<br/>Srinivas P.</body></html>"
-m.sendHTMLMail("srinivas.2447.p@gmail.com",html)
+m.sendHTMLMail("jasonchristian1234@gmail.com",html)
+print("Your message has been sent.")
 m.close()
