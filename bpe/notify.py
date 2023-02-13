ea66eab94fa673b55bb1dffe88d0a99b3d3fa885
Jason Christian <70122753+jasonchriscodes@users.noreply.github.com>
2023-02-13 21:39:25 +1300

    chore: add list successful PO in email

--- bpe/notify.py
+++ bpe/notify.py
@@ -1,94 +1,111 @@
 from lib.mail import Mailing
 import datetime
 
+
 class Notification:
 
-    def __init__(self,mail_type,to_email,cc_email,user_name,pass_word):
+    def __init__(self, mail_type, to_email, cc_email, user_name, pass_word, successOrder):
         self.to_email = to_email
         self.cc_email = cc_email
         self.mail_type = mail_type
-        self.user_name = user_name        
+        self.user_name = user_name
         self.pass_word = pass_word
         self.digest_log = []
         self.EOM = "199012"
         self.totalPOs = 0
         self.allGood = 0
         self.monkeyWrench = 0
+        self.successOrder = successOrder
 
-    def set_Success_Fail(self,goodie,bumpie):
+    def set_Success_Fail(self, goodie, bumpie):
         self.allGood += goodie
         self.monkeyWrench += bumpie
 
-    def set_EOM(self,eom):
+    def set_EOM(self, eom):
         self.EOM = eom
 
-    def set_tPO(self,allofthem):
+    def set_tPO(self, allofthem):
         self.totalPOs = allofthem
 
-    def no_lines_notify(self,po_num):
-        ##m = Mailing(self.mail_type,self.user_name,self.pass_word)
-        ##m.setSubject("AutoNotify::No lines for PO:" + str(po_num))
-        ##html = "<html><head></head><body>Hi User!<br/><br/>It is observed that Purchase Order:"+str(po_num)+" does not have any lines.<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
-        ##m.sendHTMLMail(self.to_email,html)
-        ##m.close()
-        self.add_to_digest( "no lines", po_num, str(po_num)+" <b>Missing lines: <br/>")
-        self.set_Success_Fail(0,1)
+    def no_lines_notify(self, po_num):
+        # m = Mailing(self.mail_type,self.user_name,self.pass_word)
+        # m.setSubject("AutoNotify::No lines for PO:" + str(po_num))
+        # html = "<html><head></head><body>Hi User!<br/><br/>It is observed that Purchase Order:"+str(po_num)+" does not have any lines.<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
+        # m.sendHTMLMail(self.to_email,html)
+        # m.close()
+        self.add_to_digest("no lines", po_num, str(
+            po_num)+" <b>Missing lines: <br/>")
+        self.set_Success_Fail(0, 1)
 
-    def totals_did_not_match_notify(self, po_num,details_total,doc_total):
-        ##m = Mailing(self.mail_type,self.user_name,self.pass_word)
-        ##m.setSubject("AutoNotify::Totals do not match for PO:" + str(po_num))
-        ##html = "<html><head></head><body>Hi User!<br/><br/>It is observed that Purchase Order["+str(po_num)+"]'s totals do not match.<br/><br/> <b>Details Total:</b>"+str(details_total)+"<br/><b>Document Total:</b>"+str(doc_total)+"<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
-        ##m.sendHTMLMail(self.to_email,html)
-        ##m.close()
-        self.add_to_digest( "totals mismatch", po_num, str(po_num)+" <b>Total mismatch:</b> <b>Details Total:</b>"+str(details_total)+"<br/><b>Document Total:</b>"+str(doc_total)+"<br/>")
-        self.set_Success_Fail(0,1)
-        
+    def totals_did_not_match_notify(self, po_num, details_total, doc_total):
+        # m = Mailing(self.mail_type,self.user_name,self.pass_word)
+        # m.setSubject("AutoNotify::Totals do not match for PO:" + str(po_num))
+        # html = "<html><head></head><body>Hi User!<br/><br/>It is observed that Purchase Order["+str(po_num)+"]'s totals do not match.<br/><br/> <b>Details Total:</b>"+str(details_total)+"<br/><b>Document Total:</b>"+str(doc_total)+"<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
+        # m.sendHTMLMail(self.to_email,html)
+        # m.close()
+        self.add_to_digest("totals mismatch", po_num, str(po_num)+" <b>Total mismatch:</b> <b>Details Total:</b>" +
+                           str(details_total)+"<br/><b>Document Total:</b>"+str(doc_total)+"<br/>")
+        self.set_Success_Fail(0, 1)
+
     def publish_failed_notify(self, po_num):
-        ##m = Mailing(self.mail_type,self.user_name,self.pass_word)
-        ##m.setSubject("AutoNotify::Failed publishing the PO:" + str(po_num))
-        ##html = "<html><head></head><body>Hi User!<br/><br/>It is observed that publishing of Purchase Order:"+str(po_num)+" had failed.<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
-        ##m.sendHTMLMail(self.to_email,html)
-        ##m.close()
-        self.add_to_digest( "publish failed", po_num, str(po_num)+" <b>failed.</b><br/>")
-        self.set_Success_Fail(0,1)
+        # m = Mailing(self.mail_type,self.user_name,self.pass_word)
+        # m.setSubject("AutoNotify::Failed publishing the PO:" + str(po_num))
+        # html = "<html><head></head><body>Hi User!<br/><br/>It is observed that publishing of Purchase Order:"+str(po_num)+" had failed.<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
+        # m.sendHTMLMail(self.to_email,html)
+        # m.close()
+        self.add_to_digest("publish failed", po_num,
+                           str(po_num)+" <b>failed.</b><br/>")
+        self.set_Success_Fail(0, 1)
 
-    def publish_failed_notify_unknown_error(self, po_num,msg):
-        ##m = Mailing(self.mail_type,self.user_name,self.pass_word)
-        ##m.setSubject("AutoNotify::Failed publishing the PO:" + str(po_num))
-        ##html = "<html><head></head><body>Hi User!<br/><br/>It is observed that publishing of Purchase Order:"+str(po_num)+" had failed.<br/><b>Exception message:</b>"+msg+"<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
-        ##m.sendHTMLMail(self.to_email,html)
-        ##m.close()
-        self.add_to_digest( "unknown error", po_num, str(po_num)+" <b>erred with message:</b> "+msg+".<br/>")
-        self.set_Success_Fail(0,1)
+    def publish_failed_notify_unknown_error(self, po_num, msg):
+        # m = Mailing(self.mail_type,self.user_name,self.pass_word)
+        # m.setSubject("AutoNotify::Failed publishing the PO:" + str(po_num))
+        # html = "<html><head></head><body>Hi User!<br/><br/>It is observed that publishing of Purchase Order:"+str(po_num)+" had failed.<br/><b>Exception message:</b>"+msg+"<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
+        # m.sendHTMLMail(self.to_email,html)
+        # m.close()
+        self.add_to_digest("unknown error", po_num, str(
+            po_num)+" <b>erred with message:</b> "+msg+".<br/>")
+        self.set_Success_Fail(0, 1)
 
     def add_to_digest(self, er_type, po_num, msg):
-        this_msg = [er_type,po_num,msg];
-        self.digest_log.append(this_msg);
+        this_msg = [er_type, po_num, msg]
+        self.digest_log.append(this_msg)
 
     def outbox_Msgs1(self):
         dt = datetime.datetime.now()
         print("test 1")
-        if(self.digest_log):
+        if (self.digest_log):
             print("test if 1")
             print("sent to "+self.to_email)
             html = "<html><head></head><body>Hi User,<br/><br/>"
-            #html += "Here is the digest for the ap automation on "+dt.strftime('%c')+" for "+self.EOM+".<br/><br/>"
-            html += "Automation triggered at "+dt.strftime('%c')+" with the EOM cutoff set as "+self.EOM+".<br/><br/>"
-            #html += "A total of "+str(self.totalPOs)+" were looked into and following POs has problems.<br/><br/>"
-            html +=  str(self.totalPOs)+" invoices processed from Ferret via AP Automation<br/><br/>"
-            html += str(self.allGood)+" processed are OK with no issues.<br/><br/>"
-            html +=  "But "+str(self.monkeyWrench)+" of the following issues were encountered <br/><br/>"
+            # html += "Here is the digest for the ap automation on "+dt.strftime('%c')+" for "+self.EOM+".<br/><br/>"
+            html += "Automation triggered at " + \
+                dt.strftime('%c')+" with the EOM cutoff set as " + \
+                self.EOM+".<br/><br/>"
+            # html += "A total of "+str(self.totalPOs)+" were looked into and following POs has problems.<br/><br/>"
+            html += str(self.totalPOs) + \
+                " invoices processed from Ferret via AP Automation<br/><br/>"
+            html += str(self.allGood) + \
+                " processed are OK with no issues.<br/><br/>"
+            if(len(self.successOrder)):
+                for i in range(0, len(self.successOrder)):
+                    html += "PO number:" + self.successOrder[i] + "<br/>"
+                html += "<br/>"
+            else:
+                html += "PO number: no successful PO found.<br/><br/>"
+            html += "But "+str(self.monkeyWrench) + \
+                " of the following issues were encountered <br/><br/>"
             for indx, item in enumerate(self.digest_log):
                 html += item[2]
             html += "<br/><br/>Thank You,<br/>Regards,<br/>Automation Script.</body></html>"
-            m = Mailing("gmail",self.user_name,self.pass_word)
-            m.setSubject("AutoNotify::Mail digest from AP Automation for cutoff "+self.EOM)
-            succss = m.sendHTMLMail(self.to_email,self.cc_email,html)
+            m = Mailing("gmail", self.user_name, self.pass_word)
+            m.setSubject(
+                "AutoNotify::Mail digest from AP Automation for cutoff "+self.EOM)
+            succss = m.sendHTMLMail(self.to_email, self.cc_email, html)
             m.close()
-            if(succss):
+            if (succss):
                 print("test if 2")
                 return 1
             else:
                 print("else 2")
                 return 0
-            
--- lib/Gen.py
+++ lib/Gen.py
@@ -1,143 +1,157 @@
-from pywinauto.application import Application 
+from pywinauto.application import Application
 from pywinauto import backend
-from pywinauto import mouse,keyboard
+from pywinauto import mouse, keyboard
 from pywinauto import win32structures
 import time
-import os,sys
+import os
+import sys
+
 
 class Obj:
 
-    def __init__(self,win_name,par=None,l=None):
+    def __init__(self, win_name, par=None, l=None):
         try:
             self.g = Gen(backend)
             self.win_name = win_name
-            if(not par):
+            print("self.win")
+            if (not par):
                 self.app_handle = self.g.get_handle(win_name)
                 self.g.set_window(self.app_handle)
                 self.win = self.g.get_window()
-                self.win.set_focus()     
+                self.win.set_focus()
             self.maxattempts = 10
             self.minwait = 0.5
             self.l = l
         except Exception as e:
-            if(l):
-                self.l.add('Problem occured finding window:' + win_name)
-            raise Exception('Problem occured finding window:' + win_name)
-    def setLog(self,log):
-        self.l = log
-    def click_element(self,ele):
-        rect = ele.rectangle()
-        x = int(round((rect.right - rect.left)/2,0) + rect.left)
-        y = int(round((rect.bottom - rect.top)/2,0) + rect.top)
-        mouse.click(coords=(x,y))
+            if (l):
+                self.l.add('Problem occured finding window:' +
+                           win_name + ', error: ' + str(e))
+            raise Exception('Problem occured finding window:' +
+                            win_name + ', error: ' + str(e))
 
-    def clear_field(self,ele):
+    def setLog(self, log):
+        self.l = log
+
+    def click_element(self, ele):
+        rect = ele.rectangle()
+        x = int(round((rect.right - rect.left)/2, 0) + rect.left)
+        y = int(round((rect.bottom - rect.top)/2, 0) + rect.top)
+        mouse.click(coords=(x, y))
+
+    def clear_field(self, ele):
         self.click_element(ele)
         keyboard.SendKeys('^a^c')
         keyboard.SendKeys('{BACKSPACE}')
 
-    def type_element(self,ele,text):
+    def type_element(self, ele, text):
         self.click_element(ele)
         keyboard.SendKeys(text)
 
-    def type_and_tab(self,ele,text):
-        self.type_element(ele,text)
-        self.type_element(ele,'{TAB}')
+    def type_and_tab(self, ele, text):
+        self.type_element(ele, text)
+        self.type_element(ele, '{TAB}')
 
-    def set_object(self,ele):
+    def set_object(self, ele):
         self.ele = ele
 
-    def pcis(self,dep=None):
-        if(dep):
+    def pcis(self, dep=None):
+        if (dep):
             self.win.print_control_identifiers(depth=dep)
-        else: 
+        else:
             self.win.print_control_identifiers()
 
-    def kids(self,controltype):
+    def kids(self, controltype):
         childs = self.win.children(control_type=controltype)
         print('Number of childs found:' + str(len(childs)))
         for child in childs:
             print(child.element_info.name)
 
-    def child_window_handle(self,controltype):
+    def child_window_handle(self, controltype):
         childs = self.win.children(control_type=controltype)
         for child in childs:
             print(child.element_info.name)
-            if(child.element_info.name==self.win_name):                
+            if (child.element_info.name == self.win_name):
                 self.app_handle = child.element_info.handle
-                self.win = child                
+                self.win = child
                 break
+
     def print_win_name(self):
         print(self.win.element_info.name)
 
 
 class Gen:
-    def __init__(self,backend):
+    def __init__(self, backend):
         self.backend = backend
         self.spaces = ""
-        #print("Gen object created")
+        # print("Gen object created")
 
-    def start_app(self,path):
+    def start_app(self, path):
         self.app = Application().start(path)
 
     def kill_app(self):
         self.app.kill()
-        
-    def get_handle(self,appname):
+
+    def get_handle(self, appname):
         handle = 0
         maxtries = 10
         currtry = 0
-        while (handle==0 and currtry<maxtries):
+        while (handle == 0 and currtry < maxtries):
             root = self.backend.registry.backends["uia"].element_info_class()
             handle = 0
             for w in root.children():
                 print(w.name)
-                if((w.name==appname) or (appname in w.name)):
-                    handle=w.handle
+                if ((w.name == appname) or (appname in w.name)):
+                    handle = w.handle
                     break
-            time.sleep(1)  
-            currtry = currtry + 1      
-        return handle    
+            time.sleep(1)
+            currtry = currtry + 1
+            print('handle: ', handle)
+        return handle
 
     def print_windows(self):
-        root = self.backend.registry.backends["uia"].element_info_class()    
+        root = self.backend.registry.backends["uia"].element_info_class()
         for w in root.children():
-            print(w.name)  
+            print(w.name)
 
-    def set_window(self,handle_app):
-        #print("Handle:"+ str(handle_app))
+    def set_window(self, handle_app):
+        # print("Handle:"+ str(handle_app))
         self.handle_app = handle_app
         self.app = Application(backend="uia").connect(handle=handle_app)
         self.win = self.app.top_window()
         self.win.set_focus()
+        print("self.app: ", self.app)
+        print("self.win: ", self.win)
 
     def get_window(self):
         return self.win
-    
+
     def print_win_tree(self):
         spaces = "-"
         self.objs = {}
         print(spaces)
-        self.print_child_ele_details(spaces,self.win)
-        
-    def print_child_ele_details(self,spaces,root):
+        self.print_child_ele_details(spaces, self.win)
+
+    def print_child_ele_details(self, spaces, root):
         eles = root.children()
         for ele in eles:
             rTxt = str(ele.element_info.rich_text)
-            if(rTxt and not (rTxt in self.objs)):
+            if (rTxt and not (rTxt in self.objs)):
                 out = spaces + "["
-                out = out + "control_type:" + str(ele.element_info.control_type)+","
+                out = out + "control_type:" + \
+                    str(ele.element_info.control_type)+","
                 out = out + "name:" + str(ele.element_info.name)+","
-                out = out + "class_name:" + str(ele.element_info.class_name)+","
-                out = out + "control_id:" + str(ele.element_info.control_id)+","
+                out = out + "class_name:" + \
+                    str(ele.element_info.class_name)+","
+                out = out + "control_id:" + \
+                    str(ele.element_info.control_id)+","
                 out = out + "rich_text:" + str(ele.element_info.rich_text)
                 out = out + "]"
                 print(out)
                 out = ""
-                self.objs[rTxt]  = rTxt
-            self.print_child_ele_details(spaces+"-",ele)
-    
-    def print_ele_details(self,ele):
+                self.objs[rTxt] = rTxt
+            self.print_child_ele_details(spaces+"-", ele)
+
+    def print_ele_details(self, ele):
         out = self.spaces + "["
         out = out + "control_type:" + str(ele.element_info.control_type)+","
         out = out + "name:" + str(ele.element_info.name)+","
@@ -146,8 +160,9 @@ class Gen:
         out = out + "rich_text:" + str(ele.element_info.rich_text)
         out = out + "]"
         print(out)
-        out = ""        
-    def get_elements(self,obj_type):
+        out = ""
+
+    def get_elements(self, obj_type):
         eles = self.win.children(control_type=obj_type)
         for ele in eles:
             self.print_ele_details(ele)
